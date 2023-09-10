2e8d397ed4279414a361452e14ca5e51f82b67ef
bsilvers64 <psynogen64@gmail.com>
2023-09-10 03:13:48 -0700

    task creator added with inputfield and todolist components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from "react";
-import "./App.css";
-import InputField from "./components/InputField";
-
+import React, {useState} from "react"
+import "./App.css"
+import InputField from "./components/InputField"
+import {Todo} from './model'
+import TodoList from "./components/TodoList"
 
 const App: React.FC = () => {
-  return <div className="App">
+ 
+ const [todo, setTodo] = useState<string>("")
+ const [todos, setTodos] = useState<Todo[]>([])
+
+ const handleAdd = (e: React.FormEvent) => {
+   e.preventDefault();
+
+   if(todo) {
+    setTodos([...todos, {id: Date.now(), todo, isDone: false}])
+    setTodo("")
+   }
+ }
+ console.log(todos)
+ return <div className="App">
     <span className="heading"> Taskit </span>
-    <InputField/>
+
+    <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+  
+    <TodoList todos={todos} setTodos={setTodos}></TodoList>
   </div>;
 }
 
